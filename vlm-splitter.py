1bce748619762df6646d21cac7407273d27e0e4c
harishpapineni0659 <92444500+harishpapineni0659@users.noreply.github.com>
2021-10-27 19:03:04 +0530

    Updated code

--- vlm-splitter.py
+++ vlm-splitter.py
@@ -1,7 +1,8 @@
-#codeupdate123tes26123estr354567commit
+[19:00] Carlos Alberto Olvera RODRIGUEZ
+#codeupdate123tes26123estr3
 import json
 import boto3
-from PyPDF4 import PdfFileReader, PdfFileWriter
+"""from PyPDF4 import PdfFileReader, PdfFileWriter
 import os
 from io import BytesIO
 from datetime import date, time, datetime
@@ -9,12 +10,9 @@ import psycopg2
 import uuid
 import logging
 import warnings
-
 warnings.filterwarnings("ignore")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-
 def rds_connection():
     rds_host = 'bh-dcap.cysf64wiobfj.us-east-1.rds.amazonaws.com'
     name = "bhadmin"
@@ -28,64 +26,53 @@ def rds_connection():
         logger.info("Error while connecting to PostgreSQL"+ error)
         connection.close()
         logger.info("RDS connection is closed")
-
-    return connection
-
-
+    return connection"""
 def lambda_handler(event, context):
     # bucket = event['bucket_name']
     # key = event['file_name']
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
 
-    # RDS Connection
+    """# RDS Connection
     conn = rds_connection()
     cursor = conn.cursor()
     table_name = 'vlm_spilt_pdf'
     cursor.execute("select * from information_schema.tables where table_name=%s", ('vlm_spilt_pdf',))
     if bool(cursor.rowcount) == False:
-        create_table_query = """CREATE TABLE {} (ID VARCHAR(64) PRIMARY KEY,
+        create_table_query = CREATE TABLE {} (ID VARCHAR(64) PRIMARY KEY,
                             FILENAME TEXT NOT NULL, PAGECOUNT INT,
                             DATE timestamp NOT NULL, SPLIT_FILES_PATH TEXT,
                             ARCHIVE_FILES_PATH TEXT, SPLIT_STATUS TEXT NOT NULL
-                            );""".format(table_name)
+                            );.format(table_name)
         cursor.execute(create_table_query)
         conn.commit()
         logger.info("Table created successfully in PostgreSQL ")
     else:
         logger.info("Table exists already ")
-
     query = "INSERT INTO vlm_spilt_pdf (ID, FILENAME, PAGECOUNT, DATE, SPLIT_FILES_PATH, ARCHIVE_FILES_PATH, SPLIT_STATUS) VALUES (%s, %s, %s, %s, %s, %s, %s)"
     try:
         bucket = event['Records'][0]['s3']['bucket']['name']
         key = event['Records'][0]['s3']['object']['key']
-        
         # bucket = event['bucket_name']
         # key = event['file_name']
-        
-        
         dirpath = os.path.dirname(key)
         logger.info("Connecting to " + bucket + " S3 bucket")
         s3 = boto3.resource('s3')
         client = boto3.client('s3')
         my_bucket = s3.Bucket(bucket)
-
         logger.info("Date time folder path")
         filedt = datetime.utcnow()
         fileDate = filedt.strftime('%Y-%m-%d-%H-%M-%S')
-
         logger.info("Copying & deleting of VLM pdf files from landing zone to archive vlm folder")
         for my_bucket_object in my_bucket.objects.filter(Prefix=dirpath):
             filepath = str(my_bucket_object.key)
             filename = filepath.split("/")[-1]
             print(filename)
-
             if filepath.endswith(".pdf"):
                 targetkey = "landing-zone/archive-vlm/" + fileDate + "/" + filename
                 response1 = client.copy_object(Bucket=bucket, Key=targetkey,
                                                CopySource={'Bucket': bucket, 'Key': filepath})
                 logger.info("Copying of VLM pdfs is done. Filename is" + filename)
-
                 logger.info("Splitting pdf file has been started.. Filename is:" + filename)
                 result = client.get_object(Bucket=bucket, Key=filepath)
                 body = result['Body'].read()
@@ -101,13 +88,11 @@ def lambda_handler(event, context):
                     split_key_object = str(
                         "processing-zone/split-pdfs/" + fileDate + "/" + filename[:-4] + "_page_" + page_num + ".pdf")
                     my_bucket.upload_file(tmp_path, split_key_object)
-
                     if os.path.exists(tmp_path):
                         os.remove(tmp_path)
                         logger.info("Removed the file %s" % tmp_path)
                     else:
                         logger.info("Sorry, file %s does not exist." % tmp_path)
-
                 logger.info("Splitting pdf file has been successfully finished. Filename is: " + filename)
                 response2 = client.delete_object(Bucket=bucket, Key=filepath)
                 logger.info("Delete of PO pdfs from trigger zone is done. Filename is" + filename)
@@ -116,31 +101,27 @@ def lambda_handler(event, context):
                        "Success")
                 cursor.execute(query, tbl)
                 conn.commit()
-
             elif filepath.endswith(".json"):
                 logger.info("No Json file is found. filename is" + filename)
-
             else:
                 logger.info("No file found in trigger-folder")
-
         return {
             'statusCode': 200,
             'body': json.dumps('Successful..!!')
         }
-
     except Exception as e:
         logger.info("Splitting process failed " + str(repr(e)))
         tbl = (str(uuid.uuid4().time), filename, '', datetime.utcnow(), '', '', "Failure")
         cursor.execute(query, tbl)
         conn.commit()
-
         return {
             'statusCode': 500,
             'body': json.dumps('Splitting failed ..!!')
         }
-
     finally:
         if (conn):
             cursor.close()
             conn.close()
-            logger.info("RDS connection is closed")
+            logger.info("RDS connection is closed")"""
+
+
